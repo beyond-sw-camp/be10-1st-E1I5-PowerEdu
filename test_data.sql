2a6f1223cfdd3858e0b207abac4f84afe27eaf4f
jinskore <ohjinseok2412@gmail.com>
2024-07-29 12:27:50 +0900

    feat: test_data 추가

---
+++ test_data.sql
@@ -0,0 +1,255 @@
+-- USER
+INSERT INTO user
+ VALUES
+(NULL, 'edu@gmail.com', '0000', '파워에듀', '김파워', 'F', '1985-11-12', '010-4444-4444', 0, 0, 1, '2024-07-23 17:34:44'),
+(NULL, 'eunseo@gmail.com', '1234', 'x은서x', '이은서', 'F', '1999-06-14', '010-1234-5678', 1, 0, 1, NULL),
+(NULL, 'user3@gmail.com', 'abcd', 'minji', '박민지', 'F', '1990-01-01', '010-3333-3333', 1, 1, 0, NULL),
+(NULL, 'user4@gmail.com', 'efgh', 'soohyun', '김수현', 'M', '1995-02-02', '010-4444-4444', 0, 0, 0, NULL),
+(NULL, 'user5@gmail.com', 'ijkl', 'jihye', '정지혜', 'F', '1987-03-03', '010-5555-5555', 1, 0, 1, NULL),
+(NULL, 'user6@gmail.com', 'mnop', 'dongwook', '이동욱', 'M', '1992-04-04', '010-6666-6666', 0, 1, 1, NULL),
+(NULL, 'user7@gmail.com', 'qrst', 'jisu', '최지수', 'F', '1989-05-05', '010-7777-7777', 1, 0, 0, '2023-12-03 03:05:11'),
+(NULL, 'user8@gmail.com', 'uvwx', 'hyunjin', '한현진', 'M', '1991-06-06', '010-8888-8888', 0, 1, 0, NULL),
+(NULL, 'user9@gmail.com', 'yzab', 'eunji', '강은지', 'F', '1988-07-07', '010-9999-9999', 1, 0, 1, NULL),
+(NULL, 'user10@gmail.com', 'cdef', 'minho', '최민호', 'M', '1993-08-08', '010-1010-1010', 0, 1, 1, NULL),
+(NULL, 'user11@gmail.com', '1234', 'soyeon', '김소연', 'F', '1985-11-12', '010-1111-1111', 1, 0, 1, NULL),
+(NULL, 'user12@gmail.com', '5678', 'taeyang', '이태양', 'M', '1990-01-12', '010-1212-1212', 1, 1, 0, NULL),
+(NULL, 'user13@gmail.com', 'abcd', 'eunjoo', '박은주', 'F', '1995-02-13', '010-1313-1313', 0, 0, 1, NULL),
+(NULL, 'user14@gmail.com', 'efgh', 'junseo', '최준서', 'M', '1991-03-14', '010-1414-1414', 1, 0, 0, NULL),
+(NULL, 'user15@gmail.com', 'ijkl', 'hyunwoo', '김현우', 'M', '1987-04-15', '010-1515-1515', 0, 1, 1, NULL),
+(NULL, 'user16@gmail.com', 'mnop', 'yeji', '이지혜', 'F', '1989-05-16', '010-1616-1616', 1, 0, 0, NULL),
+(NULL, 'user17@gmail.com', 'qrst', 'sumin', '박수민', 'F', '1992-06-17', '010-1717-1717', 0, 1, 1, NULL),
+(NULL, 'user18@gmail.com', 'uvwx', 'jungwoo', '정정우', 'M', '1993-07-18', '010-1818-1818', 1, 0, 0, NULL),
+(NULL, 'user19@gmail.com', 'yzab', 'haeun', '김하은', 'F', '1994-08-19', '010-1919-1919', 0, 1, 1, NULL),
+(NULL, 'user20@gmail.com', 'cdef', 'seojun', '이서준', 'M', '1986-09-20', '010-2020-2020', 1, 0, 0, NULL),
+(NULL, 'user21@gmail.com', '1234', 'jinyoung', '정진영', 'M', '1987-11-21', '010-2121-2121', 1, 1, 1, '2024-05-12 12:15:05'),
+(NULL, 'user22@gmail.com', '5678', 'hyerin', '최혜린', 'F', '1990-12-22', '010-2222-2222', 0, 0, 0, NULL),
+(NULL, 'user23@gmail.com', 'abcd', 'donghyun', '김동현', 'M', '1995-01-23', '010-2323-2323', 1, 0, 1, NULL),
+(NULL, 'user24@gmail.com', 'efgh', 'soojin', '박수진', 'F', '1991-02-24', '010-2424-2424', 0, 1, 0, NULL),
+(NULL, 'user25@gmail.com', 'ijkl', 'wonbin', '이원빈', 'M', '1987-03-25', '010-2525-2525', 1, 0, 1, NULL),
+(NULL, 'user26@gmail.com', 'mnop', 'yuna', '한유나', 'F', '1992-04-26', '010-2626-2626', 0, 1, 0, NULL),
+(NULL, 'user27@gmail.com', 'qrst', 'joon', '최준', 'M', '1989-05-27', '010-2727-2727', 1, 0, 1, NULL),
+(NULL, 'user28@gmail.com', 'uvwx', 'gyuri', '강규리', 'F', '1991-06-28', '010-2828-2828', 0, 1, 0, NULL);
+
+-- LECTURE
+INSERT INTO LECTURE
+ VALUES
+(NULL, 1, '입문자바', '이것은 입문자바에 대한 강의입니다.', 100, '하', 10000, 0),
+(NULL, 4, '기본자바', '이것은 기본자바에 대한 강의입니다.', 300, '중', 23000, 0),
+(NULL, 6, '심화자바', '이것은 심화자바에 대한 강의입니다.', 500, '상', 47000, 0),
+(NULL, 8, '입문파이썬', '이것은 입문파이썬에 대한 강의입니다.', 120, '하', 15000, 0),
+(NULL, 10, '기본파이썬', '이것은 기본파이썬에 대한 강의입니다.', 370, '중', 37000, 0),
+(NULL, 13, '심화파이썬', '이것은 심화파이썬에 대한 강의입니다.', 480, '상', 52000, 0),
+(NULL, 15, '입문c++', '이것은 입문c++에 대한 강의입니다.', 150, '하', 18000, 0),
+(NULL, 17, '기본c++', '이것은 기본c++에 대한 강의입니다.', 390, '중', 34000, 0);
+
+-- LECTURE_INTERST
+INSERT INTO interest_lecture
+ VALUES
+   (NULL, 1, 1)
+ , (NULL, 1, 3)
+ , (NULL, 2, 10)
+ , (NULL, 2, 15)
+ , (NULL, 4, 2)
+ , (NULL, 7, 16);
+ 
+-- REVIEW_POST
+INSERT INTO REVIEW_POST
+VALUES
+(NULL, 2, 1, '피카추', '입문자바 후기', '강의 내용이 체계적이고 이해하기 쉬웠습니다.', 5, NOW(), 0, DEFAULT),
+(NULL, 3, 2, '라이츄', '기본자바', '강사가 친절하게 설명해주셔서 좋았습니다.', 4, NOW(), 0, DEFAULT),
+(NULL, 5, 3, '루피', '심화자바', '강의가 매우 흥미로웠고 많은 것을 배웠습니다.', 5, NOW(), 0, DEFAULT),
+(NULL, 7, 4, '뽀로로', '입문파이썬', '다시 듣고 싶을 정도로 만족스러웠습니다.', 4, NOW(), 0, DEFAULT),
+(NULL, 9, 5, '가렌', '기본파이썬', '내용이 조금 어렵게 느껴졌습니다.', 3, NOW(), 0, DEFAULT);
+
+-- REVIEW_LIKE
+INSERT INTO review_like
+VALUES
+(NULL, 5, 1),  -- 사용자 1이 후기 5에 좋아요
+(NULL, 4, 2),  -- 사용자 2가 후기 4에 좋아요
+(NULL, 3, 3),  -- 사용자 3이 후기 3에 좋아요
+(NULL, 2, 4),  -- 사용자 4가 후기 2에 좋아요
+(NULL, 1, 5);  -- 사용자 5가 후기 1에 좋아요
+
+-- STUDY_GROUP
+INSERT INTO `STUDY_GROUP` (
+    `STUDY_GRP_LEADER_ID`, 
+    `STUDY_GRP_TITLE`, 
+    `STUDY_GRP_CONTENT`, 
+    `STUDY_GRP_MAX_MEMBER`, 
+    `STUDY_GRP_LINK`
+) VALUES 
+(1, 'AI Study Group', 'This is a study group for learning AI.', 10, 'http://aistudygroup.com'),
+(2, 'Data Science Group', 'This group focuses on data science projects.', 15, 'http://datasciencegroup.com'),
+(3, 'Web Development Group', 'A group for web development enthusiasts.', 20, 'http://webdevgroup.com'),
+(4, 'Machine Learning Group', 'Exploring machine learning techniques.', 12, 'http://mlgroup.com'),
+(5, 'Blockchain Study Group', 'Discussing blockchain technology and applications.', 8, 'http://blockchaingroup.com');
+
+-- STUDY_GROUP_APPLICATION
+INSERT INTO `STUDY_GROUP_APPLICATION` (
+    `STUDY_GRP_APPLICATION_ID`, 
+    `STUDY_GRP_ID`, 
+    `USER_ID`,
+    `STATUS`
+) VALUES 
+(1, 1, 3, 0),
+(2, 1, 4, 0),
+(3, 2, 5, 0),
+(4, 2, 6, 0),
+(5, 3, 7, 0),
+(6, 3, 8, 0),
+(7, 4, 9, 0),
+(8, 4, 10, 0),
+(9, 5, 1, 0),
+(10, 5, 2, 0);
+
+
+
+-- QUESTION_POST
+INSERT INTO question_post
+VALUES
+(1, 2, 1, '별명1', '이 부분이 이해가 안가요', '평소에도 궁금한 내용이었는데, 강의보고 이해가 안가서 질문 남깁니다.', '2021-04-18 15:27', 0),
+(2, 3, 1, '별명2', 'JAVA에서의 형 변환', '소괄호와 함께하면 괜찮을까요? 아니면, parse를 써야할까요?', '2021-03-19 12:13', 0),
+(3, 7, 4,  '별명3','파이썬 아나콘다', '용량이 너무 많이 차지해서, 다른걸로 하려는데 추천 부탁드립니다.', '2022-04-13 9:01', 0),
+(4, 9, 3,  '별명4','멀티 스레딩의 존재 여부', '멀티 스레딩을 왜 하는지 궁금합니다. JVM은 하나의 프로세서에서 진행되고, 스레드를 분할하면 효율이 적어지지 않나요?', '2023-07-09 00:53', 0);
+
+-- QUESTION_COMMENT
+INSERT INTO question_comment
+VALUES
+(1, 1, 4, '별명1', '오 저도 이거 이해안가요!!', '2021-04-18 15:27', 0),
+(2, 2, 2, '별명2', '이거는 과거 자바 8 이전에 대해서 알아야 합니다.', '2021-03-19 12:13', 0),
+(3, 3, 5, '별명3', '으르르렁', '2022-04-13 9:01', 0),
+(4, 4, 7, '별명4', '멀티 스레딩을 왜 하는지 궁금합니다.', '2023-07-09 00:53', 0);
+
+-- PAY_HISTORY
+INSERT
+ INTO
+    `PAY_HISTORY`(`USER_ID`, `PAY_DATE`, `PRICE`, `PAY_METHOD`, `PAY_CONFIRM_ID`)
+VALUES 
+(2, NOW(), 15000, '계좌이체', '2024725abcddd1'),
+(3, NOW(), 20000, '신용카드', '2024725abcddd2'),
+(5, NOW(), 30000, '현금', '2024725abcddd3'),
+(7, NOW(), 25000, '계좌이체', '2024725abcddd4'),
+(9, NOW(), 18000, '신용카드', '2024725abcddd5');
+
+-- PURCHASED_LECTURE
+INSERT INTO `PURCHASED_LECTURE` (
+    `PAY_HISTORY_ID`, 
+    `BUYER_ID`, 
+    `LECTURE_ID`
+) VALUES 
+(1, 2, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 3, 4),
+(5, 5, 5);
+
+-- CHAPTER
+INSERT INTO chapter
+(LECTURE_ID, CHAPTER_TITLE, CHAPTER_TIME, CHAPTER_NUMBER, REFERENCE_PATH)
+VALUES
+(1, '변수, 상수, 데이터타입', 20, 1, 'https://java_1.kr/'),
+(1, '조건문, 반복문, 함수', 40, 2, 'https://java_2.kr/'),
+(3, '클래스, 상속', 60, 1, 'https://java_3.kr/'),
+(3, '인터페이스, 예외 처리', 60, 2, 'https://java_4.kr/'),
+(7, 'C++ 개념 파악', 40, 1, 'https://C++_1.kr/'),
+(7, 'C++ 활용하기', 40, 2, 'https://C++_2.kr/'),
+(7, 'C++ 실습하기', 70, 3, 'https://C++_3.kr/');
+
+-- RENTAL_LIST
+INSERT INTO rental_list
+( 
+  USER_ID
+, LECTURE_ID
+, CHAPTER_ID
+, PAY_ID
+, RENTAL_START
+, RENTAL_END
+, TARGET_DATE
+, RENTAL_STATUS
+)
+VALUES
+(
+  1
+, 1
+, 1
+, 1
+, '2024-07-15'
+, DATE_ADD('2024-07-15', INTERVAL (SELECT
+													   rental_day
+  												 FROM rental_price_tbl
+ 												WHERE rental_price_id = 1) DAY)
+, NULL
+, DEFAULT
+);
+
+-- RENTAL_PRICE_TBL
+INSERT
+  INTO rental_price_tbl (RENTAL_DAY, RENTAL_PRICE)
+VALUES
+(3, 3000),
+(6, 5000),
+(9, 8000),
+(3, 5000),
+(6, 8000),
+(9, 12000);
+
+-- CHAPTER_PRICE
+INSERT INTO CHAPTER_PRICE (CHAPTER_PRICE_ID, CHAPTER_ID, RENTAL_PRICE_ID)
+VALUES
+    (NULL, 1, 5),
+    (NULL, 2, 4),
+    (NULL, 3, 3),
+    (NULL, 4, 2),
+    (NULL, 5, 1);
+
+-- NOTIFICATION
+INSERT INTO NOTIFICATION
+VALUES
+  (NULL, 1, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 2, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 3, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 4, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 5, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 6, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 7, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 8, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 9, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 10, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 11, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 12, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 13, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 14, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 15, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 16, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 17, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 18, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 19, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 20, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 21, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 22, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 23, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 24, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 25, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 26, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 27, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 28, 'N', 0, '서비스 점검 공지!! 2024년 7월 24일 오전 01시~02시까지 모든 서비스 중단합니다')
+, (NULL, 1, 'W', 0, '시청 목표일이 하루 남았습니다! 오늘은 꼭 같이 공부해요!')
+, (NULL, 3, 'W', 0, '시청 목표일이 3일 남았습니다! 힘을 내요!')
+, (NULL, 6, 'W', 0, '시청 목표일이 3일 남았습니다! 힘을 내요!')
+, (NULL, 10, 'W', 0, '시청 목표일이 일주일 남았습니다! 화이팅~')
+, (NULL, 15, 'W', 0, '시청 목표일이 일주일 남았습니다! 화이팅~')
+, (NULL, 21, 'W', 0, '시청 목표일이 일주일 남았습니다! 화이팅~');
+
+-- STUDY_GROUP_MEMBER
+INSERT INTO `STUDY_GROUP_MEMBER` (
+    `STUDY_GRP_MEMBER_ID`, 
+    `STUDY_GRP_ID`,
+    `USER_ID`,
+    `ROLE`
+) VALUES 
+(NULL, 1, 1, 'M'),
+(NULL, 2, 2, 'M'),
+(NULL, 3, 3, 'M'),
+(NULL, 4, 4, 'M'),
+(NULL, 5, 5, 'M');
+
